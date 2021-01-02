7ea0cdaf25a80daa32b64f0486972cd70b3a4a51
endorsystems <esane.demay@gmail.com>
2021-01-02 12:57:15 -0500

    updated

--- scripts/vpn_status.sh
+++ scripts/vpn_status.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#printf " " && ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'    -- `ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'`
+#printf " " && ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'
 
 if [ "$(pgrep fortivpn)" ]; then
-        printf " " && ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'    -- `ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'`
+        printf " Connected: " && ip a | grep ppp0 | grep inet | awk -F' ' '{print $2}'
 else
         echo " Disconnected"
 fi
